0b61da338a60b2791164a26de840552fd817ea36
Alfred Tuva <muindetuva@gmail.com>
2023-01-29 23:08:01 +0300

    Complete posts index page

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <div class="mb-3">
+        <h1 class="text-2xl font-bold text-center mt-3">
+            All Posts
+        </h1>
+        <p class="text-center text-xs">Latest tech trends, our opinions and our thoughts.</p>
+    </div>
+
+    <div class="flex flex-col">
+        <x-post></x-post>
+        <x-post></x-post>
+        <x-post></x-post>
+        <x-post></x-post>
+    </div>
+</x-layout>
