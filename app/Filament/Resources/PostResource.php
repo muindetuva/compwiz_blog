cbfeb8d602c7cd354bab9d815304dba9e71ddb00
Alfred Tuva <muindetuva@gmail.com>
2023-01-30 01:44:50 +0300

    Redirect to the posts index page on save

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -44,6 +44,7 @@ class PostResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
