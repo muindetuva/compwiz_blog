3888b2465a4defe5fd8b06f99130d9f4fd042fd5
Alfred Tuva <muindetuva@gmail.com>
2023-01-30 09:44:08 +0300

    Only add user id to Post model being created if there's a logged in user

--- app/Traits/FilterByUser.php
+++ app/Traits/FilterByUser.php
@@ -13,9 +13,13 @@ trait FilterByUser
 
         $user_id = Auth::id() ?? null;
 
-        self::creating(function ($model) use ($user_id) {
-            $model->user_id = $user_id;
-        });
+        // Only add user_id if there's a logged in user
+        if (Auth::hasUser()) {
+            self::creating(function ($model) use ($user_id) {
+                $model->user_id = $user_id;
+            });
+        }
+
 
         self::addGlobalScope('user', function (Builder $builder) use ($user_id) {
             $builder->where('user_id', $user_id);
